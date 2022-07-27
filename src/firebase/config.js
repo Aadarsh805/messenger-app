667edd73ace53dea611f9a004c5be99866e594a1
Aadarsh Thakur <aadarshthakur80501@gmail.com>
2022-07-27 19:00:13 +0530

    routes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,13 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
-export const auth = firebase.initializeApp({
+export const auth = firebase
+  .initializeApp({
     apiKey: "AIzaSyCQydL8s9eUH2C-JaB0huQ506NSL1KttMY",
-  authDomain: "messenger-app-b0af2.firebaseapp.com",
-  projectId: "messenger-app-b0af2",
-  storageBucket: "messenger-app-b0af2.appspot.com",
-  messagingSenderId: "1089653729749",
-  appId: "1:1089653729749:web:ab37d38fc99c1d3ba8755c"
-}).auth
+    authDomain: "messenger-app-b0af2.firebaseapp.com",
+    projectId: "messenger-app-b0af2",
+    storageBucket: "messenger-app-b0af2.appspot.com",
+    messagingSenderId: "1089653729749",
+    appId: "1:1089653729749:web:ab37d38fc99c1d3ba8755c",
+  })
+  .auth();
